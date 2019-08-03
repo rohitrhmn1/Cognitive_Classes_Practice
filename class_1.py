02f28e7b2d9bd5f33c276c874030124a97c93bd4
rohit <rohitrhmn208@gmail.com>
2019-08-03 22:33:48 +0530

    String operations Tuple and lists

--- class_1.py
+++ class_1.py
@@ -4,3 +4,15 @@ import string
 a = "This is a string"
 b = a.upper()
 print(b)
+asc = string.ascii_letters
+print(asc)
+Numbers = "0123456"
+print(Numbers[::2])
+print("0123456".find('1'))
+Letters = "ABCDEFGHIJK"
+print(Letters[0:4])
+Good = "GsoAo+d"
+print(Good[::2])
+print("uppercase".upper())
+
+# Tuples and Lists
