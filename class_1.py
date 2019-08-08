20f58c7e7b105b92d2e2690f1c63692643379231
rohit <rohitrhmn208@gmail.com>
2019-08-08 23:19:41 +0530

    Created numpy programs

--- class_1.py
+++ class_1.py
@@ -23,11 +23,37 @@
 
 # Working with Pandas
 
-import pandas as pd
+# import pandas as pd
+#
+# csv_path = "File1.csv"
+# # df = pd.read_csv(csv_path)  # df stands for DataFrame
+# # df.head()
+# df = pd.DataFrame({'a': [11, 21, 31], 'b': [21, 22, 23]})
+# print(df.head(3))
+# print(df['b'])
 
-csv_path = "File1.csv"
-# df = pd.read_csv(csv_path)  # df stands for DataFrame
-# df.head()
-df = pd.DataFrame({'a': [11, 21, 31], 'b': [21, 22, 23]})
-print(df.head(3))
-print(df['b'])
+# Working with Numpy Array
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array([1, 2, 3, 4, 2147483647])
+print(type(a))
+print("Mean: ", a.mean())
+print(a.dtype)
+print(a.ndim)
+print(a.size)
+b = np.array([3.1, 2.10, 4.33, 5.6, 7.8])
+print(type(b))
+print(b.dtype)
+x = np.array([np.pi, np.pi / 2, 0])
+print(x)
+y = np.sin(x)
+print(y)
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.sin(x)
+print(x)
+print(y)
+plt.plot(x, y)
+plt.show()
+z = np.linspace(0, 255)
+print(z)
