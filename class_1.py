f16d14d6dd2d2b7773e47273670c2d54f621bae1
rohit <rohitrhmn208@gmail.com>
2019-08-08 19:17:33 +0530

    Working with files

--- class_1.py
+++ class_1.py
@@ -1,18 +1,22 @@
 # String Operations
-import string
-
-a = "This is a string"
-b = a.upper()
-print(b)
-asc = string.ascii_letters
-print(asc)
-Numbers = "0123456"
-print(Numbers[::2])
-print("0123456".find('1'))
-Letters = "ABCDEFGHIJK"
-print(Letters[0:4])
-Good = "GsoAo+d"
-print(Good[::2])
-print("uppercase".upper())
+# import string
+#
+# a = "This is a string"
+# b = a.upper()
+# print(b)
+# asc = string.ascii_letters
+# print(asc)
+# Numbers = "0123456"
+# print(Numbers[::2])
+# print("0123456".find('1'))
+# Letters = "ABCDEFGHIJK"
+# print(Letters[0:4])
+# Good = "GsoAo+d"
+# print(Good[::2])
+# print("uppercase".upper())
 
 # Tuples and Lists
+# Files
+with open(".\\FileOne\\Files\\test_1.txt", "w") as file1:
+    file1.write("Your text goes here")
+file1.close()
